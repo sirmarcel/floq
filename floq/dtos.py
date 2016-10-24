579bd09616bbb4c60289f93a424c121b351cbeb4
sirmarcel <me@lumenlog.com>
2016-10-24 14:53:40 +0100

    add some documentation to FloquetProblemParameters

--- floq/dtos.py
+++ floq/dtos.py
@@ -2,12 +2,15 @@ import numpy as np
 
 class FloquetProblemParameters(object):
     """Data transfer object to hold the parameters of a Floquet problem"""
-    def __init__(self,dim=None,zones=None,components=None,omega=None,t=None,decimals=10):
-        self.dim = dim
-        self.zones = zones
-        self.components = components
-        self.omega = omega
-        self.t = t
-        self.decimals = decimals
+    def __init__(self,dim=None,zones=None,components=None,controls=None,omega=None,t=None,decimals=10):
+        self.dim = dim # Size of the Hilbert space of the physical system
+        self.zones = zones # Number of Brillouin zones in the extended space
+        self.components = components # Number of frequency components of Hf
+        self.controls = controls # Number of control parameters
+
+        self.omega = omega # The frequency associated with the period of the control signal, T = 2 Pi / omega
+        self.t = t # Control duration
         
-        self.k_dim = dim*zones
+        self.decimals = decimals # Number of decimals used for internal rounding
+        
+        self.k_dim = dim*zones # Size of the extended Hilbert + Fourier space
