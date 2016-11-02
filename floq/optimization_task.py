b1341bed1c57b67dfdf10499cc8a936863683f76
Marcel Langer <me@sirmarcel.com>
2016-11-02 15:26:18 +0100

    Start on OptimzationTask

---
+++ floq/optimization_task.py
@@ -0,0 +1,50 @@
+import numpy as np
+import floq.parametric_system as ps
+import floq.evolution as ev
+
+class OptimizationTaskBase(object):
+    """
+    Base class to specify an optimization problem, i.e. a physical (parametrised) system, a fidelity, a target and an initial control vector.
+    """
+    def system(self):
+        raise NotImplementedError
+
+    def fidelity(self,control,t):
+        raise NotImplementedError
+
+    def grad_fidelity(self,control,t):
+        raise NotImplementedError
+
+    def target_fidelity(self,control,t):        
+        raise NotImplementedError
+
+    def target(self):
+        raise NotImplementedError
+
+    def target_fid(self):
+        raise NotImplementedError
+
+class OptimizationTaskWithFunctions(OptimizationTaskBase):
+    def __init__(self,system,fid,dfid,target,target_fid,a0):
+        """
+        system: Instance of ParametricSystem*
+        fid: callable fid(u,target)
+        dfid: callable fid(u,du,target)
+        """
+
+        self.system = system
+        self.fid = fid
+        self.dfid = dfid
+        self.target = target
+        self.target_fid = target_fid
+        self.a0 = a0
+
+    def fidelity(controls,t):
+        fixed = system.get_system(controls,t)
+        u = ev.evolve_system(fixed)
+        return self.fid(u,self.target)
+
+    def grad_fidelity(controls,t):
+        fixed = system.get_system(controls,t)
+        [u,du] = ev.evolve_system_with_derivatives(fixed)
+        return self.dfid(u,du,self.target)
