292531dadb1ac9eed8834f95628c2b7a9dfa0dd5
Marcel Langer <me@sirmarcel.com>
2016-11-03 16:08:34 +0100

    Added a fidelity that can be MINIMISED.
    And it kind of works!

--- examples/optimize_rabi.py
+++ examples/optimize_rabi.py
@@ -12,13 +12,13 @@ s = rb.get_rabi_system([1.2,2.8],5.0)
 print ev.evolve_system(s.get_system([0.5],1.0))
 
 target = np.array([[0.0374008 - 0.883626j, 0.223747 - 0.409566j], [0.456211 + 0.0983779j, -0.693177 - 0.549271j]])
-init = np.array([0.5])
+init = np.array([0.1])
 
-task = ot.OptimizationTaskWithFunctions(s,fid.trace_overlap,fid.d_trace_overlap,target,0.99,init)
+task = ot.OptimizationTaskWithFunctions(s,fid.overlap_to_minimise,fid.d_overlap_to_minimise,target,0.99,init)
 
 
 optimizer = opt.SciPyOptimizer(task,1.0)
 # 
 
 res = optimizer.optimize()
-pdb.set_trace()
+print res
--- floq/fidelity.py
+++ floq/fidelity.py
@@ -7,6 +7,13 @@ def trace_overlap(system,u,target):
 def d_trace_overlap(system,u,dus,target):
     return np.array([trace_overlap(system,du,target) for du in dus])
 
+def overlap_to_minimise(system,u,target):
+    return 1.0 - np.abs(trace_overlap(system,u,target))
+
+def d_overlap_to_minimise(system,u,dus,target):
+    df = d_trace_overlap(system,u,dus,target)
+    return -df*np.sign(df)
+
 def hilbert_schmidt_product(a,b):
     a = np.matrix(a)
     return np.trace(np.dot(a.getH(),b))
