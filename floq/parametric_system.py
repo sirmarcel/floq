0d397f09d27464399e857c344048e7c1b5c83c74
Marcel Langer <me@sirmarcel.com>
2016-11-04 15:07:15 +0100

    parametric_system.py is pep8 compliant

--- floq/parametric_system.py
+++ floq/parametric_system.py
@@ -1,31 +1,36 @@
 import floq.fixed_system as fs
 
+
 class ParametericSystemBase(object):
     """
-    Base class to specify a physical system that still has open parameters, such as the control amplitudes, the control duration, or other arbitrary parameters in the Hamiltonian.
+    Base class to specify a physical system that still has open parameters,
+    such as the control amplitudes, the control duration, or other arbitrary
+    parameters in the Hamiltonian.
 
     This needs to be sub-classed, and a subclass should provide:
-    - get_system(controls), which needs to return a FixedSystem instance. This will be called during optimisation.
+    - get_system(controls)
 
     """
-    
-    def get_system(self,controls,t):
+
+    def get_system(self, controls, t):
         raise NotImplementedError("get_system not implemented.")
+
 
 class ParametricSystemWithFunctions(ParametericSystemBase):
     """
-    A system with parametric hf and dhf, which are passed as callables to the constructor. 
+    A system with parametric hf and dhf, which are passed as callables to the constructor.
 
     hf has to have the form hf(a,parameters)
     """
 
-    def __init__(self,hf,dhf,nz,omega,parameters):
+    def __init__(self, hf, dhf, nz, omega, parameters):
         """
         hf: callable hf(controls,parameters,omega)
         dhf: callable dhf(controls,parameters,omega)
         omega: 2 pi/T, the period of the Hamiltonian
         nz: number of Fourier modes to be considered during evolution
-        parameters: a data structure that holds parameters for hf and dhf (dictionary is probably the best idea)
+        parameters: a data structure that holds parameters for hf and dhf
+        (dictionary is probably the best idea)
         """
         self.hf = hf
         self.dhf = dhf
@@ -33,13 +38,13 @@ class ParametricSystemWithFunctions(ParametericSystemBase):
         self.nz = nz
         self.parameters = parameters
 
-    def calculate_hf(self,controls):
-        return self.hf(controls,self.parameters,self.omega)
+    def calculate_hf(self, controls):
+        return self.hf(controls, self.parameters, self.omega)
 
-    def calculate_dhf(self,controls):
-        return self.dhf(controls,self.parameters,self.omega)
+    def calculate_dhf(self, controls):
+        return self.dhf(controls, self.parameters, self.omega)
 
-    def get_system(self,controls,t):
+    def get_system(self, controls, t):
         hf = self.calculate_hf(controls)
         dhf = self.calculate_dhf(controls)
-        return fs.FixedSystem(hf,dhf,self.nz,self.omega,t)
+        return fs.FixedSystem(hf, dhf, self.nz, self.omega, t)
