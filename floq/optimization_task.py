afcad3de48fe179183ba1f7ce68980f7e551d753
Marcel Langer <me@sirmarcel.com>
2016-11-04 15:02:57 +0100

    optimization_task.py is pep8 compliant

--- floq/optimization_task.py
+++ floq/optimization_task.py
@@ -2,20 +2,23 @@ import numpy as np
 import floq.parametric_system as ps
 import floq.evolution as ev
 
+
 class OptimizationTaskBase(object):
     """
-    Base class to specify an optimization problem, i.e. a physical (parametrised) system, a fidelity, a target and an initial control vector.
+    Base class to specify an optimization problem,
+    i.e. a physical (parametrised) system,
+    a fidelity, a target and an initial control vector.
     """
     def system(self):
         raise NotImplementedError
 
-    def fidelity(self,control,t):
+    def fidelity(self, control, t):
         raise NotImplementedError
 
-    def grad_fidelity(self,control,t):
+    def grad_fidelity(self, control, t):
         raise NotImplementedError
 
-    def target_fidelity(self,control,t):        
+    def target_fidelity(self, control, t):
         raise NotImplementedError
 
     def target(self):
@@ -24,8 +27,9 @@ class OptimizationTaskBase(object):
     def target_fid(self):
         raise NotImplementedError
 
+
 class OptimizationTaskWithFunctions(OptimizationTaskBase):
-    def __init__(self,system,fid,dfid,target,target_fid,init):
+    def __init__(self, system, fid, dfid, target, target_fid, init):
         """
         system: Instance of ParametricSystemBase
         fid: callable fid(fixed_system,u,target)
@@ -39,12 +43,12 @@ class OptimizationTaskWithFunctions(OptimizationTaskBase):
         self.target_fid = target_fid
         self.init = init
 
-    def fidelity(self,controls,t):
-        fixed = self.system.get_system(controls,t)
+    def fidelity(self, controls, t):
+        fixed = self.system.get_system(controls, t)
         u = ev.evolve_system(fixed)
-        return self.fid(fixed,u,self.target)
+        return self.fid(fixed, u, self.target)
 
-    def grad_fidelity(self,controls,t):
-        fixed = self.system.get_system(controls,t)
-        u,du = ev.evolve_system_with_derivatives(fixed)
-        return self.dfid(fixed,u,du,self.target)
+    def grad_fidelity(self, controls, t):
+        fixed = self.system.get_system(controls, t)
+        u, du = ev.evolve_system_with_derivatives(fixed)
+        return self.dfid(fixed, u, du, self.target)
