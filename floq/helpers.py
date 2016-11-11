4a08122110f0876031bd0d1aa535a83a2c23a9e0
Marcel Langer <me@sirmarcel.com>
2016-11-11 16:53:50 +0100

    Add rollover to num_to_i

--- floq/helpers.py
+++ floq/helpers.py
@@ -4,11 +4,18 @@ def n_to_i(num, n):
     -(n-1)/2 through (n-1)/2
     into an index i from 0 to n-1
 
+    If num > (n-1)/2, map it into the interval
+
     This is necessary to translate from a physical
     Fourier mode number to an index in an array.
     """
     cutoff = (n-1)/2
-    return num+cutoff
+    if num > cutoff:
+        return (num % n)-n+cutoff
+    if num < -cutoff:
+        return -(-num % n)+n+cutoff
+    else:
+        return num+cutoff
 
 
 def i_to_n(i, n):
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -17,6 +17,18 @@ class TestFourierIndexToNormalIndex(unittest.TestCase):
     def test_in_between(self):
         self.assertEqual(h.n_to_i(-3, 81), 37)
 
+    def test_too_big_a_bit(self):
+        self.assertEqual(h.n_to_i(5, 7), h.n_to_i(-2, 7))
+
+    def test_too_big_a_lot(self):
+        self.assertEqual(h.n_to_i(5+7, 7), h.n_to_i(-2, 7))
+
+    def test_too_small_a_bit(self):
+        self.assertEqual(h.n_to_i(-6, 7), h.n_to_i(1, 7))
+
+    def test_too_small_a_lot(self):
+        self.assertEqual(h.n_to_i(-6-14, 7), h.n_to_i(1, 7))
+
 
 class TestNormalIndexToFourierIndex(unittest.TestCase):
 
