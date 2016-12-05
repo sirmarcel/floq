caca5c4d2341ac2e79d2a43e7f508372562ab081
Marcel Langer <me@sirmarcel.com>
2016-12-05 10:21:03 +0000

    Update benchmark to use more fundamental spin functions

--- benchmark/u_and_du.py
+++ benchmark/u_and_du.py
@@ -1,7 +1,8 @@
 import sys
 sys.path.append('..')
 import numpy as np
-import floq.systems.spins as spins
+import floq.core.spin as spin
+import floq.core.fixed_system as fs
 import timeit
 
 
@@ -22,43 +23,43 @@ def time_du(func, hf, dhf, params):
 
 
 ncomp = 6
-n = 1
-freqs = 1.1*np.ones(n)
-amps = 1.0*np.ones(n)
-s = spins.SpinEnsemble(n, ncomp, 1.5, freqs, amps)
-
+freq = 1.1
 controls = 0.5*np.ones(2*ncomp)
-s.set_nz(controls, 1.5)
-system = s.get_systems(controls, 1.5)[0]
+
+hf = spin.hf(ncomp, freq, controls)
+dhf = spin.dhf(ncomp, 1.0)
+system = fs.FixedSystem(hf, dhf, 101, 1.0, 1.0)
+
+params = system.params
 
 print "---- Current version (Numba)"
 import floq.core.evolution as ev
 
-print time_u(ev.get_u, system.hf, system.params)
-print time_du(ev.get_u_and_du, system.hf, system.dhf, system.params)
+print time_u(ev.get_u, hf, params)
+print time_du(ev.get_u_and_du, hf, dhf, params)
 
 
 print "---- Better algorithm for dU"
 import floq.museum.p4.evolution as ev
 
-print time_u(ev.get_u, system.hf, system.params)
-print time_du(ev.get_u_and_du, system.hf, system.dhf, system.params)
+print time_u(ev.get_u, hf, params)
+print time_du(ev.get_u_and_du, hf, dhf, params)
 
 
 print "---- Use less Python"
 import floq.museum.p3.evolution as ev
 
-print time_u(ev.get_u, system.hf, system.params)
-print time_du(ev.get_u_and_du, system.hf, system.dhf, system.params)
+print time_u(ev.get_u, hf, params)
+print time_du(ev.get_u_and_du, hf, dhf, params)
 
 
 print "---- Use sparse matrix library"
 import floq.museum.p2.evolution as ev
 
-print time_u(ev.get_u, system.hf, system.params)
-print time_du(ev.get_u_and_du, system.hf, system.dhf, system.params)
+print time_u(ev.get_u, hf, params)
+print time_du(ev.get_u_and_du, hf, dhf, params)
 print "---- Baseline"
 import floq.museum.p1.evolution as ev
 
-print time_u(ev.get_u, system.hf, system.params)
-print time_du(ev.get_u_and_du, system.hf, system.dhf, system.params)
+print time_u(ev.get_u, hf, params)
+print time_du(ev.get_u_and_du, hf, dhf, params)
