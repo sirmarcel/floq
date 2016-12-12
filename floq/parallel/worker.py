a82be5c3d09ec86274d5cd56cb8b46bf1ba4387a
Marcel Langer <me@sirmarcel.com>
2016-12-12 11:37:34 +0000

    Light cleanup of worker.py

--- floq/parallel/worker.py
+++ floq/parallel/worker.py
@@ -1,39 +1,18 @@
-# You got nothing to lose but your chains!
 import multiprocessing as mp
 import numpy as np
 from floq.optimization.fidelity import FidelityComputerBase
-
-
-class FidelityWorker(mp.Process):
-    def __init__(self, fids, pipe_in, pipe_out):
-        super(FidelityWorker, self).__init__()
-        self.pipe_in = pipe_in
-        self.pipe_out = pipe_out
-        self.fids = fids
-        print 'Worker initialised with ' + str(len(self.fids)) + ' fidelities'
-
-
-    def run(self):
-        while True:
-            msg = self.pipe_in.recv()
-            if msg is not None:
-                instruction, ctrl = msg[0], msg[1]
-                # print msg
-                if instruction == 'f':
-                    f = np.sum([fid.f(ctrl) for fid in self.fids])
-                    self.pipe_out.send(f)
-                else:
-                    df = np.sum([fid.df(ctrl) for fid in self.fids], axis=0)
-                    self.pipe_out.send(df)
-            else:
-                break
 
 
 
 class FidelityMaster(FidelityComputerBase):
     """
     With a given Ensemble, and a FidelityComputer,
-    calculate the average fidelity over the whole ensemble.
+    calculate the average fidelity over the whole ensemble
+    by distributing the work onto nworker sub-processes, which
+    should run in parallel if there are enough cores available.
+
+    Note: After use, the FidelityMaster should be forced to kill the child processes
+    by calling the kill() method. It is recommended to use a new FidelityMaster thereafter.
     """
 
     def __init__(self, nworker, ensemble, fidelity, **params):
@@ -41,13 +20,12 @@ class FidelityMaster(FidelityComputerBase):
         self.n = len(ensemble.systems)
         self.nworker = nworker
 
-        # chunk_size = self.n/nworker + self.n % nworker  # divide and round up, with integers
-        # print chunk_size
         self.fidelities_chunked = chunks(self.fidelities, nworker)
         self._make_workers()
 
 
     def _make_workers(self):
+        # Spawn the workers, set up pipes to talk to them
         in_pipes = [mp.Pipe() for i in xrange(self.nworker)]
         out_pipes = [mp.Pipe() for i in xrange(self.nworker)]
 
@@ -62,6 +40,7 @@ class FidelityMaster(FidelityComputerBase):
 
 
     def f(self, controls_and_t):
+        """ Compute the average fidelity of the ensemble """
         for pipe in self.ins:
             pipe.send(['f', controls_and_t])
 
@@ -73,6 +52,7 @@ class FidelityMaster(FidelityComputerBase):
 
 
     def df(self, controls_and_t):
+        """ Compute the average gradient of the fidelity of the ensemble """
         for pipe in self.ins:
             pipe.send(['df', controls_and_t])
 
@@ -84,17 +64,55 @@ class FidelityMaster(FidelityComputerBase):
 
 
     def kill(self):
+        """ Terminate the workers spawned"""
+        for pipe in self.ins:
+            pipe.send(None)  # tell workers to stop run()
         for worker in self.workers:
-            worker.terminate()
+            worker.terminate()  # shut them down
+
+
+
+class FidelityWorker(mp.Process):
+    """
+    Wraps a list of FidelityComputers, performing their computations in a separate
+    process. Communication with the 'master' process is done via Pipes. F and dF will
+    be added up locally, since only averages are used in the optimisation. This reduces
+    the amount of data to be communicated between processes.
+
+    Note: the start() methods needs to be run before computations are performed.
+    """
+
+    def __init__(self, fids, pipe_in, pipe_out):
+        super(FidelityWorker, self).__init__()
+        self.pipe_in = pipe_in
+        self.pipe_out = pipe_out
+        self.fids = fids
+        print 'Worker initialised with ' + str(len(self.fids)) + ' fidelities'
+
+
+    def run(self):
+        # When this is run, the Worker starts listening on its in_pipe,
+        # it stops when None is sent through the pipe.
+        while True:
+            msg = self.pipe_in.recv()
+            if msg is not None:
+                instruction, ctrl = msg[0], msg[1]
+                if instruction == 'f':
+                    f = np.sum([fid.f(ctrl) for fid in self.fids])
+                    self.pipe_out.send(f)
+                else:
+                    df = np.sum([fid.df(ctrl) for fid in self.fids], axis=0)
+                    self.pipe_out.send(df)
+
+            else:
+                break
 
 
 
 def chunks(l, n):
     """
-    Split list l into n-sized chunks.
+    Split list l into n chunks as uniformly as possible.
     """
-    # n = max(1, n)
-    # return [l[i:i+n] for i in xrange(0, len(l), n)]
 
     k, m = divmod(len(l), n)
     return list((l[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in xrange(n)))
