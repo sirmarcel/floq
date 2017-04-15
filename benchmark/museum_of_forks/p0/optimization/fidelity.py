e7ec192552bc8957d2bd5ba1466384bcd294be18
Marcel Langer <me@sirmarcel.com>
2017-04-15 18:46:43 +0100

    Updated legacy fidelity to make benchmarks not break

--- benchmark/museum_of_forks/p0/optimization/fidelity.py
+++ benchmark/museum_of_forks/p0/optimization/fidelity.py
@@ -7,12 +7,20 @@ import numpy as np
 class FidelityComputerBase(object):
     """
     Define how to calculate a fidelity and its gradient from a
-    ParametricSystem.
+    ParametricSystem. Keeps track of iterations, counting up every
+    time a new controls_and_t array is encountered.
 
     This is a base class and needs to be sub-classed. Sub-classes
-    should implement f(controls_and_t) and df(controls_and_t), where
+    should implement _f(controls_and_t) and _df(controls_and_t), where
     controls_and_t is an array of control amplitudes and possibly
     the duration of the pulse as last entry.
+
+    Optionally, a sub-class can implement a function penalty(controls_and_t) and
+    d_penalty(controls_and_t) that represent a penalty function that is added
+    to f and df, respectively.
+
+    Also optionally, a function _iteterate can be implemented, which gets called
+    on each iteration.
 
     The __init__ should take the form __init__(self, system, **kwargs)
     for compatibility with EnsembleFidelity.
@@ -20,14 +28,51 @@ class FidelityComputerBase(object):
 
     def __init__(self, system):
         self.system = system
+        self._last_controls_and_t = None
+        self.iterations = 0
 
 
     def f(self, controls_and_t):
-        raise NotImplementedError
+        return self._f(controls_and_t) + self.penalty(controls_and_t)
 
 
     def df(self, controls_and_t):
+        return self._df(controls_and_t) + self.d_penalty(controls_and_t)
+
+
+    def iterate(self, controls_and_t):
+        """
+        Gets called by the Optimizer after each iteration. Increases
+        the iteration count self.iterations, and calls the (optional)
+        _iterate method.
+        """
+        self.iterations += 1
+        self._iterate(controls_and_t)
+        f = self.f(controls_and_t)
+
+
+    def reset_iterations(self):
+        self.iterations = 0
+
+
+    def _f(self, controls_and_t):
         raise NotImplementedError
+
+
+    def _df(self, controls_and_t):
+        raise NotImplementedError
+
+
+    def _iterate(self, controls_and_t):
+        pass
+
+
+    def penalty(self, controls_and_t):
+        return 0.0
+
+
+    def d_penalty(self, controls_and_t):
+        return 0.0
 
 
 
@@ -38,16 +83,18 @@ class EnsembleFidelity(FidelityComputerBase):
     """
 
     def __init__(self, ensemble, fidelity, **params):
+        super(EnsembleFidelity, self).__init__(ensemble)
         self.fidelities = [fidelity(sys, **params) for sys in ensemble.systems]
 
-    def f(self, controls_and_t):
+    def _f(self, controls_and_t):
         f = np.mean([fid.f(controls_and_t) for fid in self.fidelities])
         return f
 
 
-    def df(self, controls_and_t):
+    def _df(self, controls_and_t):
         df = np.mean([fid.df(controls_and_t) for fid in self.fidelities], axis=0)
         return df
+
 
 
 
